8cd623d6b7122a1db4fdfd2d51facd23ce0b15cd
sritunjungs <48472264+sritunjungs@users.noreply.github.com>
2019-04-09 14:54:26 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=UQ1SUPITfDBWSwn3FSV2kap7Nf8reTsLxadvpoV5";
         
         $("#loadImage").click(function(){
             $.ajax({
